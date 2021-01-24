4f63946f562531c1b8a925870e3781cbd547f278
Daniel Floyd <34635302+gleekzorp@users.noreply.github.com>
2021-01-24 15:17:24 -0700

    Remove bootstrap, remove app and import drumMachine direct to index

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
-import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles.css";
 
-import App from "./App";
+import DrumMachine from "./components/DrumMachine";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <DrumMachine />
     </Provider>
   </React.StrictMode>,
   rootElement
