37e85057d4b1a1729443b797b2e52f5c325b5360
Daniel Floyd <34635302+gleekzorp@users.noreply.github.com>
2021-01-24 18:28:26 -0700

    Add font awesome logo, change font family, update readme

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # FreeCodeCamp Drum Machine
 
-This was part of freeCodeCamps Front End Libraries Certificate. Styling and functionality matches the following: https://codepen.io/freeCodeCamp/full/MJyNMd
-
-# TODO:
-
-- Power and Bank words darkend
+This was part of the freeCodeCamp Front End Libraries Certificate. Styling and functionality matches the following: https://codepen.io/freeCodeCamp/full/MJyNMd
--- src/components/DrumMachine.js
+++ src/components/DrumMachine.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFreeCodeCamp } from "@fortawesome/free-brands-svg-icons";
 
 import DrumPadContainer from "./drums/DrumPadContainer";
 import ControlsContainer from "./controls/ControlsContainer";
@@ -6,6 +8,9 @@ import ControlsContainer from "./controls/ControlsContainer";
 const DrumMachine = () => {
   return (
     <div className="container" id="drum-machine">
+      <div className="logo">
+        FCC <FontAwesomeIcon icon={faFreeCodeCamp} className="icon" />
+      </div>
       <DrumPadContainer />
       <ControlsContainer />
     </div>
